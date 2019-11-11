77f723852e3b224e37ff055689326c32df0a8790
NikHuang <nikhhq@sina.cn>
2019-11-12 00:47:08 +0800

    123

---
+++ src/main/java/com/huang/order/MainApplication.java
@@ -0,0 +1,12 @@
+package com.huang.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
